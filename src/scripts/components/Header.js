089fbc7621d3c1f5fa2c1ce6714c377b49c9c2f0
jonsbaa <joanna_mehtala@hotmail.com>
2018-01-03 21:35:25 +0200

    feat: initial Header

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import DataStore from "flux/stores/DataStore.js";
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
 
 class Header extends React.Component {
   render() {
@@ -21,32 +24,35 @@ class Header extends React.Component {
     const menuPages = allPages.sort(compare);
 
     return (
-      <Nav>
-        <Link to="/" style={{ marginRight: "10px" }}>
-          Home
-        </Link>
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/" style={{ marginRight: "10px" }}>
+              [ logo ]
+            </Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          {menuPages.map(page => {
+            if (page.slug != "home") {
+              return (
+                <NavItem key={page.id}>
+                  <Link
+                    key={page.id}
+                    to={`/${page.slug}`}
+                    style={{ marginRight: "10px" }}
+                  >
+                    {page.title.rendered}
+                  </Link>
+                </NavItem>
+              );
+            }
+          })}
+        </Nav>
 
-        {menuPages.map(page => {
-          if (page.slug != "home") {
-            return (
-              <Link
-                key={page.id}
-                to={`/${page.slug}`}
-                style={{ marginRight: "10px" }}
-              >
-                {page.title.rendered}
-              </Link>
-            );
-          }
-        })}
-      </Nav>
+      </Navbar>
     );
   }
 }
-
-const Nav = styled.div`
-  width: 100%;
-  overflow: hidden;
-`;
 
 export default Header;
